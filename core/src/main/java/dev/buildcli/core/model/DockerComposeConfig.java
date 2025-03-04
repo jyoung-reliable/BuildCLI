afe1842bced1d9be867855c90b2010b1d0572a5f
uluizeduardo <luiz.analistadesistemas@gmail.com>
2025-03-03 22:46:29 -0300

    feat(DockerCompose): created DockerComposeConfig record

--- core/src/main/java/dev/buildcli/core/model/DockerComposeConfig.java
+++ core/src/main/java/dev/buildcli/core/model/DockerComposeConfig.java
@@ -3,17 +3,17 @@ package dev.buildcli.core.model;
 import java.util.List;
 
 public record DockerComposeConfig(
+        String imageName,
         List<String> ports,
         List<String> volumes,
         String cpu,
-        String memory,
-        String dockerFilePath
+        String memory
 ) {
     public DockerComposeConfig{
-        ports          = (ports == null || ports.isEmpty()) ? List.of("8080:8080") : List.copyOf(ports);
-        volumes        = (volumes == null || volumes.isEmpty()) ? List.of() : List.copyOf(volumes);
-        cpu            = (cpu == null || cpu.isBlank()) ? "1" : cpu;
-        memory         = (memory == null || memory.isBlank()) ? "512m" : memory;
-        dockerFilePath = (dockerFilePath == null || dockerFilePath.isBlank()) ? "./Dockerfile" : dockerFilePath;
+        imageName = (imageName == null || imageName.isBlank()) ? "buildcli-app" : imageName;
+        ports     = (ports == null || ports.isEmpty()) ? List.of("8080:8080") : List.copyOf(ports);
+        volumes   = (volumes == null || volumes.isEmpty()) ? List.of() : List.copyOf(volumes);
+        cpu       = (cpu == null || cpu.isBlank()) ? "1" : cpu;
+        memory    = (memory == null || memory.isBlank()) ? "512m" : memory;
     }
 }
