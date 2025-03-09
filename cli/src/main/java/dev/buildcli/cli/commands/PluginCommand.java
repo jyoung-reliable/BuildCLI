5e926345151cf27fa281346cd81086ec1b743f4f
Dumildes Paulo <dumildesfernando@outlook.com>
2025-03-09 02:38:04 +0100

    feat: add plugin command

---
+++ cli/src/main/java/dev/buildcli/cli/commands/PluginCommand.java
@@ -0,0 +1,9 @@
+package dev.buildcli.cli.commands;
+
+import picocli.CommandLine.Command;
+
+@Command(name = "plugin", aliases = {"plg"}, description = "", mixinStandardHelpOptions = true,
+    subcommands = {}
+)
+public class PluginCommand {
+}
---
+++ cli/src/main/java/dev/buildcli/cli/commands/plugin/AddCommand.java
@@ -0,0 +1,10 @@
+package dev.buildcli.cli.commands.plugin;
+
+import dev.buildcli.core.domain.BuildCLICommand;
+
+public class AddCommand implements BuildCLICommand {
+  @Override
+  public void run() {
+
+  }
+}
