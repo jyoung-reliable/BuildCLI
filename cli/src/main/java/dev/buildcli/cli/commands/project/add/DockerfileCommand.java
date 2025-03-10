1c8ecda090b7bac458bc028f130b17aa8c6b6c19
iamsb97 <shreyankbhat97@gmail.com>
2025-03-10 13:21:42 +0530

    Add --force and --env flags for Dockerfile generation

--- cli/src/main/java/dev/buildcli/cli/commands/project/add/DockerfileCommand.java
+++ cli/src/main/java/dev/buildcli/cli/commands/project/add/DockerfileCommand.java
@@ -4,9 +4,13 @@ import dev.buildcli.core.domain.BuildCLICommand;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -23,13 +27,33 @@ public class DockerfileCommand implements BuildCLICommand {
   private String fromImage;
   @Option(names = {"--port", "-p"}, description = "", defaultValue = "8080", split = ",")
   private List<Integer> ports;
+  @Option(names = {"--env", "-e"}, description = "", defaultValue = "")
+  private String envVariable;
+  @Option(names = {"--force"}, description = "", defaultValue = "false")
+  private Boolean force;
 
   @Override
   public void run() {
     try {
       File dockerfile = new File(name);
-      if (dockerfile.createNewFile()) {
-        try (FileWriter writer = new FileWriter(dockerfile)) {
+      if (dockerfile.createNewFile() || force) {
+        try (FileWriter writer = new FileWriter(dockerfile, false)) {
+
+          String[] envVars = envVariable.split(";");
+          for (int i = 0; i < envVars.length; i++) {
+            if (envVars[i].contains("JAVA_TOOL_OPTIONS")) {
+              String java_tool_options = "";
+              java_tool_options = envVars[i].split("=")[1];
+              java_tool_options = validate_jvm_options(java_tool_options);
+              if (java_tool_options == null) {
+                dockerfile.delete();
+                return;
+              }
+              envVars[i] = new StringBuffer().append("JAVA_TOOL_OPTIONS=\"").append(java_tool_options).append("\"").toString();
+              continue;
+            }
+            envVars[i] = new StringBuffer(envVars[i].split("=")[0]).append("=\"").append(envVars[i].split("=")[1]).append("\"").toString();
+          }
 
           var builder = new StringBuilder("FROM ").append(fromImage).append("\n");
           builder.append("WORKDIR ").append("/app").append("\n");
@@ -37,6 +61,9 @@ public class DockerfileCommand implements BuildCLICommand {
           ports.forEach(port -> {
             builder.append("EXPOSE ").append(port).append("\n");
           });
+          for (String s: envVars) {
+            builder.append("ENV ").append(s).append("\n");
+          }
           builder.append("ENTRYPOINT ").append("[\"java\", \"-jar\", \"app.jar\"]").append("\n");
 
           writer.write(builder.toString());
@@ -51,5 +78,33 @@ public class DockerfileCommand implements BuildCLICommand {
       logger.log(Level.SEVERE, "Failed to setup Docker", e);
       System.err.println("Error: Could not setup Docker environment.");
     }
+  }
+
+  private String validate_jvm_options(String options) {
+    if (!options.equals("")) {
+      try {
+        List<String> command = new ArrayList<>();
+        command.add("java");
+        command.add("--dry-run");
+        command.addAll(Arrays.asList(options.split(" ")));
+        command.add("-version");
+        var processBuilder = new ProcessBuilder(command);
+        var process = processBuilder.start();
+        var code = process.waitFor();
+        if (code != 0) {
+          BufferedReader br = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+          String line;
+          while ((line = br.readLine()) != null) {
+            System.err.println(line);
+          }
+          return null;
+        }
+      } catch (Exception e) {
+        e.printStackTrace();
+        return null;
+      }
+    }
+
+    return options;
   }
 }
