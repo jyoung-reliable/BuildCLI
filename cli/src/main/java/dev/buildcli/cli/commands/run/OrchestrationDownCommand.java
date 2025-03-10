4aa8d4815b04893331044e12ab21a2e57967df25
uluizeduardo <luiz.analistadesistemas@gmail.com>
2025-03-09 23:43:05 -0300

    refactor(DockerCompose): refactored OrchestrationDownCommand class for implementing tests

--- cli/src/main/java/dev/buildcli/cli/commands/run/OrchestrationDownCommand.java
+++ cli/src/main/java/dev/buildcli/cli/commands/run/OrchestrationDownCommand.java
@@ -1,9 +1,11 @@
 package dev.buildcli.cli.commands.run;
 
 import dev.buildcli.core.domain.BuildCLICommand;
+import dev.buildcli.core.exceptions.DockerException;
 import dev.buildcli.core.utils.DockerManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
@@ -12,22 +14,31 @@ import picocli.CommandLine.Option;
 public class OrchestrationDownCommand implements BuildCLICommand {
 
     private static final Logger logger = LoggerFactory.getLogger(OrchestrationDownCommand.class.getName());
+    private final DockerManager dockerManager;
 
     @Option(names = {"--name", "-n"}, description = "Force image reconstruction")
     private String containerName;
 
+    public OrchestrationDownCommand(DockerManager dockerManager) {
+        this.dockerManager = dockerManager;
+    }
+
+    public void setContainerName(String containerName) {
+        this.containerName = containerName;
+    }
+
     @Override
     public void run() {
         try {
-            DockerManager.downContainer(containerName);
+            dockerManager.downContainer(containerName);
 
             String message = (containerName == null || containerName.isEmpty())
                     ? "All running containers have been successfully stopped."
                     : "Container '%s' has been successfully stopped.".formatted(containerName);
 
             logger.info(message);
-        } catch (Exception e) {
-            logger.error("Failed to stop containers", e);
+        } catch (DockerException | CommandLine.ExecutionException e) {
+            throw new CommandLine.ExecutionException(new CommandLine(this), "Failed to stop containers: " + e.getMessage(), e);
         }
     }
 }
