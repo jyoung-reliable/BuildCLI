bd7db3b45fd0a32759c91dc14a0bbae8a257c432
Dumildes Paulo <dumildesfernando@outlook.com>
2025-03-14 23:56:10 +0100

    feat: add list command, to list all plugins

--- plugin/src/main/java/dev/buildcli/plugin/utils/PluginUtils.java
+++ plugin/src/main/java/dev/buildcli/plugin/utils/PluginUtils.java
@@ -4,8 +4,49 @@ import dev.buildcli.core.domain.jar.Jar;
 import dev.buildcli.plugin.BuildCLICommandPlugin;
 import dev.buildcli.plugin.BuildCLIPlugin;
 
+import java.util.Optional;
+
 public final class PluginUtils {
   public static boolean isValid(Jar jar) {
     return !PluginLoader.load(BuildCLIPlugin.class, jar).isEmpty() || !PluginLoader.load(BuildCLICommandPlugin.class, jar).isEmpty();
+  }
+
+  public static Optional<String> getPluginVersion(Jar jar) {
+    var plugins = PluginLoader.load(BuildCLIPlugin.class, jar);
+    plugins.addAll(PluginLoader.load(BuildCLICommandPlugin.class, jar));
+
+    var plugin = plugins.getFirst();
+
+    if (plugin == null) {
+      return Optional.empty();
+    } else {
+      return Optional.ofNullable(plugin.version());
+    }
+  }
+
+  public static Optional<String> getPluginDescription(Jar jar) {
+    var plugins = PluginLoader.load(BuildCLIPlugin.class, jar);
+    plugins.addAll(PluginLoader.load(BuildCLICommandPlugin.class, jar));
+
+    var plugin = plugins.getFirst();
+
+    if (plugin == null) {
+      return Optional.empty();
+    } else {
+      return Optional.ofNullable(plugin.description());
+    }
+  }
+
+  public static Optional<String> getPluginName(Jar jar) {
+    var plugins = PluginLoader.load(BuildCLIPlugin.class, jar);
+    plugins.addAll(PluginLoader.load(BuildCLICommandPlugin.class, jar));
+
+     var plugin = plugins.getFirst();
+
+     if (plugin == null) {
+       return Optional.empty();
+     } else {
+       return Optional.ofNullable(plugin.name());
+     }
   }
 }
