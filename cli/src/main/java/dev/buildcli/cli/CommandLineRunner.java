800992688da017f429ebef90a25b315b3f666730
Dumildes Paulo <dumildesfernando@outlook.com>
2025-03-18 02:39:30 +0100

    fix: load subcomands class in plugin

--- cli/src/main/java/dev/buildcli/cli/CommandLineRunner.java
+++ cli/src/main/java/dev/buildcli/cli/CommandLineRunner.java
@@ -11,6 +11,7 @@ public class CommandLineRunner {
 
   public static void main(String[] args) {
     LoggingConfig.configure();
+    //PluginManager.addPluginsToClassPath();
 
     BuildCLIService.welcome();
 
