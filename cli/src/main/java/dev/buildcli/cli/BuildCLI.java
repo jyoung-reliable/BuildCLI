ef5931634997a387974eaeb1f1e1822023809a90
Wheslley Rimar Bezerra <wheslley_rimar@hotmail.com>
2025-03-22 00:14:55 -0300

    feat: add bug report command (WIP)

--- cli/src/main/java/dev/buildcli/cli/BuildCLI.java
+++ cli/src/main/java/dev/buildcli/cli/BuildCLI.java
@@ -12,7 +12,7 @@ import picocli.CommandLine.HelpCommand;
     subcommands = {
         AboutCommand.class, AiCommand.class, AutocompleteCommand.class, ChangelogCommand.class, ConfigCommand.class,
         DoctorCommand.class, HookCommand.class, ProjectCommand.class, PluginCommand.class, RunCommand.class,
-        VersionCommand.class, HelpCommand.class, ManCommand.class
+        VersionCommand.class, HelpCommand.class, BugCommand.class, ManCommand.class
     }
 )
 public class BuildCLI {
