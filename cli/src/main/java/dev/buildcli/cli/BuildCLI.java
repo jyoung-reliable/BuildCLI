6487bd48ccdb00d8761ef25315829f54d4cd4a3a
Wheslley Rimar Bezerra <wheslley_rimar@hotmail.com>
2025-03-23 18:41:26 -0300

    refactor: remove  package and implement BuildCLICommand interface

--- cli/src/main/java/dev/buildcli/cli/BuildCLI.java
+++ cli/src/main/java/dev/buildcli/cli/BuildCLI.java
@@ -2,8 +2,7 @@ package dev.buildcli.cli;
 
 import dev.buildcli.cli.commands.*;
 import dev.buildcli.cli.commands.AiCommand;
-import dev.buildcli.cli.commands.bug.BugCommand;
-import picocli.CommandLine;
+import dev.buildcli.cli.commands.BugCommand;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.HelpCommand;
 
